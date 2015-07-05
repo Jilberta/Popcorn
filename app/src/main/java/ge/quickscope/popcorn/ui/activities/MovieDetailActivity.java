96adfa747131b930d8baee1c0448b3ea9c63e420
Jilberta <gvakh10@freeuni.edu.ge>
2015-07-05 19:16:29 +0400

    daemata Activity-ebi

--- app/src/main/java/ge/quickscope/popcorn/ui/activities/MovieDetailActivity.java
+++ app/src/main/java/ge/quickscope/popcorn/ui/activities/MovieDetailActivity.java
@@ -1,23 +1,24 @@
-package ge.quickscope.popcorn;
+package ge.quickscope.popcorn.ui.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import ge.quickscope.popcorn.R;
 
-public class MainActivity extends ActionBarActivity {
+public class MovieDetailActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie_detail);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_movie_detail, menu);
         return true;
     }
 
