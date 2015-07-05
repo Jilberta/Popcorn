2527b16ebdc2b94c64887b06db88ada5262e87d3
Jilberta <gvakh10@freeuni.edu.ge>
2015-07-05 19:56:17 +0400

    დაემატა Presenter-ები MVP პატერნის გათვალისწინებით.

---
+++ app/src/main/java/ge/quickscope/popcorn/mvp/presenters/MovieCatalogPresenter.java
@@ -0,0 +1,16 @@
+package ge.quickscope.popcorn.mvp.presenters;
+
+/**
+ * Created by Jay on 7/5/2015.
+ */
+public class MovieCatalogPresenter extends Presenter {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ app/src/main/java/ge/quickscope/popcorn/mvp/presenters/Presenter.java
@@ -0,0 +1,15 @@
+package ge.quickscope.popcorn.mvp.presenters;
+
+/**
+ * Created by Jay on 7/5/2015.
+ */
+
+/**
+ * Interface representing Presenter in the MVP Pattern
+ */
+public abstract class Presenter {
+
+    public abstract void start();
+
+    public abstract void stop();
+}
